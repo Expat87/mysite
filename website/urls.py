b7930ee1b5934e79577edbfa5b41dab41f75a82f
antonallan@me.com <antonallan@me.com>
2024-08-04 19:28:50 +0800

    AJAX Working Rev 1

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('pipe_data_sch', views.pipe_data_sch, name='pipe_data_sch'),
     path('pipe_data_flange', views.pipe_data_flange, name='pipe_data_flange'),
+    path('calc_xsa', views.calc_xsa, name='calc_xsa'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from .models import AsmePipe, FlangeRating
+from django.template.loader import render_to_string
+from django.contrib import messages
 from numpy import interp
 import ast
 from typing import List, Dict, Any
 import json
+from .models import AsmePipe, FlangeRating
+
 #import os
 
 # Data directory
@@ -43,14 +46,18 @@ def pipe_data_sch(request):
                   {'asme_pipe': asme_pipe},
                   )
 
-
 def pipe_data_flange(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        DT = data.get('design_temp')
-        DP = data.get('design_press')
-        print(DT, DP)
-        # Your existing logic to calculate flange data
+        try:
+            DT = float(request.POST.get('inputDesignTemp', 65))  # Default to 65 if no input
+            DP = float(request.POST.get('inputDesignPress', 15.0))  # Default to 18.0 if no input
+        except ValueError:
+
+            messages.success(request, "Please enter a temperature between -29 and 480degC.")
+            messages.success(request, "Please enter a valid pressure.")
+            DT = 65  # Fallback to default value if input is invalid
+            DP = 15.0
+        
         flange_ratings = FlangeRating.objects.all()
         flange_data = []
         flange_ratings_set = set()
@@ -81,110 +88,71 @@ def pipe_data_flange(request):
                 'allowable': spec['acceptable']
             }
 
-        return JsonResponse({
-            'organised_flange_data': organised_flange_data,
-            'unique_flange_ratings': unique_flange_ratings,
-            'DT': DT,
-            'DP': DP,
-        })
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            table_html = render_to_string('website/pipe_data_flange_table.html', {
+                'organised_flange_data': organised_flange_data,
+                'unique_flange_ratings': unique_flange_ratings,
+            })
+            return JsonResponse({'table_html': table_html})
 
     else:
+        DT = 65  # Default diameter if not POST request
+        DP = 15.0  # Default diameter if not POST request
         
-        flange_ratings = FlangeRating.objects.all()
+        # flange_ratings = FlangeRating.objects.all()
+        # flange_data = []
+        # flange_ratings_set = set()
+        # for item in flange_ratings:
+        #     max_pressure = parse_list_from_string(item.max_pressure)
+        #     temp_range = parse_list_from_string(item.temp_range)
+        #     pipe_class = {
+        #         "material": item.material,
+        #         "flange_rating": item.flange_rating,
+        #         "max_p": max_pressure,
+        #         "temp_range": temp_range
+        #     }
+        #     flange_data.append(pipe_class)
+        #     flange_ratings_set.add(int(item.flange_rating))
 
-        # Save data to a list of dictionaries
-        flange_data = []
-        flange_ratings_set = set()
-        for item in flange_ratings:
-            max_pressure = parse_list_from_string(item.max_pressure)
-            temp_range = parse_list_from_string(item.temp_range)
-            pipe_class = {
-                "material": item.material,
-                "flange_rating": item.flange_rating,
-                "max_p": max_pressure,
-                "temp_range": temp_range
-            }
-            # Add to flange_data list
-            flange_data.append(pipe_class)
-            # Add to set of unique flange ratings
-            flange_ratings_set.add(int(item.flange_rating))
+        # unique_flange_ratings = sorted(flange_ratings_set)
+        # flange_rating_list = calc_max_p(flange_data, DT, DP)
 
-        # Convert set to sorted list
-        unique_flange_ratings = sorted(flange_ratings_set)
-        
-        # User Input DP & DT
-        DT = 230  # deg C
-        DP = 54  # barg
-
-        flange_rating_list = calc_max_p(flange_data, DT, DP)
-
-        # Organise data by material and flange rating
-        organised_flange_data = {}
-        #print(flange_rating_list)
-        for spec in flange_rating_list:
-            material = spec['material']
-            flange_rating = spec['flange_rating']
-            if material not in organised_flange_data:
-                organised_flange_data[material] = {}
-            organised_flange_data[material][flange_rating] = {'max_p':spec['calculated_max_p'],
-                                                              'design_t': DT,
-                                                              'allowable': spec['acceptable']}
+        # organised_flange_data = {}
+        # for spec in flange_rating_list:
+        #     material = spec['material']
+        #     flange_rating = spec['flange_rating']
+        #     if material not in organised_flange_data:
+        #         organised_flange_data[material] = {}
+        #     organised_flange_data[material][flange_rating] = {
+        #         'max_p': spec['calculated_max_p'],
+        #         'design_t': DT,
+        #         'allowable': spec['acceptable']
+        #     }
 
         return render(request, 'website/pipe_data_flange.html', {
-            'organised_flange_data': organised_flange_data,
-            'unique_flange_ratings': unique_flange_ratings,
+
             'DT': DT,
             'DP': DP,
         })
-##def pipe_data_flange(request):
-##    # Import engineering data
-##    flange_ratings = FlangeRating.objects.all()
-##
-##    # Save data to a list of dictionarys
-##    flange_data = []
-##    for item in flange_ratings:
-##
-##        max_pressure = item.max_pressure.split("[")[1].split(']')[0].split(",")
-##        max_pressure = [eval(i) for i in max_pressure]
-##        temp_range = item.temp_range.split("[")[1].split(']')[0].split(",")
-##        temp_range = [eval(i) for i in temp_range]
-##
-##        pipe_class = {"material" : item.material,
-##             "flange_rating": item.flange_rating,
-##             "max_p": max_pressure,
-##             "temp_range": temp_range
-##             }
-##        # Add to flange_data list
-##        flange_data.append(pipe_class)
-##    # User Input DP & DT
-##    DT = 230 #deg C
-##    DP = 54 # barg
-##
-##    def calc_max_p(pipe_spec_list, design_t, design_p):
-##        # Loop through pipe specs and calculate max pressure for each pipe spec based on design temp
-##        # For each pipe_spec in the list set X and Y co-ordinates for interpolation
-##        print(pipe_spec_list)
-##        for pipe_spec in pipe_spec_list:
-##            if design_t > max(pipe_spec['temp_range']):
-##                pipe_spec["calculated_max_p"] = 0
-##            elif design_t < -29:
-##                pipe_spec["calculated_max_p"] = 0
-##            else:
-##                x = pipe_spec['temp_range']
-##                y = pipe_spec['max_p']
-##                # Interpolate max pressure based on Design Temperature
-##                pipe_spec["calculated_max_p"] = round(interp(design_t, x, y),2)
-##            # Check if the required Design Pressure is higher than the calculated maximum
-##            if design_p > pipe_spec["calculated_max_p"]:
-##                pipe_spec["acceptable"] = False
-##            else:
-##                pipe_spec["acceptable"] = True
-##        return pipe_spec_list
-##
-##    flange_rating_list = calc_max_p(flange_data, DT, DP)
-##    
-##    return render(request, 'website/pipe_data_flange.html',
-##                  {'flange_rating_list': flange_rating_list,
-##                   'DT': DT,
-##                   'DP': DP},
-##                  )
+
+
+def calc_xsa(request):
+    if request.method == 'POST':
+        try:
+            dia = float(request.POST.get('inputDia', 25))  # Default to 25 if no input
+        except ValueError:
+            dia = 25  # Fallback to default value if input is invalid
+        pipeXSA = round(3.142 * (dia / 2) ** 2)
+        
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'pipeXSA': f"{pipeXSA:.2f}"})
+        
+    else:
+        dia = 25  # Default diameter if not POST request
+        pipeXSA = round(3.142 * (dia / 2) ** 2,3)
+
+    return render(request, 'website/calc_xsa.html', {
+        'pipeXSA': pipeXSA,
+        'dia': dia,
+    })
+
